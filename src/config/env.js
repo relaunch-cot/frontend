76fff393ffd108903f4fa5950e7f7775b5ed51e5
Matheus Mangualde <matheusmangualde@gmail.com>
2025-11-17 14:43:42 -0300

    feat: update backend URL to use HTTPS for improved security

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 const ENV_CONFIG = {
-  URL_BACKEND: 'bff-relaunch.up.railway.app',
+  URL_BACKEND: 'https://bff-relaunch.up.railway.app',
   WS_BACKEND: 'ws://bff-relaunch.up.railway.app',
   API_VERSION: 'v1',
   TIMEOUT: 10000
